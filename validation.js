eb17ae3820a7cece7508564a8e32ebc312769e37
unknown <gausulislam5@gmail.com>
2023-09-20 01:56:32 +0600

    update validation

---
+++ validation.js
@@ -0,0 +1,14 @@
+const number=23;
+
+//string ,number,boolean
+if(typeof number==='number'){
+    console.log('value is a number');
+}else{
+    console.log('value is a not a number');
+}
+
+
+const student={name:'hemo',job:'hemonto'};
+console.log(typeof student);
+const number1=[3,12,78,98];
+console.log(typeof number1);
